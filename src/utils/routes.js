2c82aa2919697f52da0f3d52becb070ecad2fee3
GTPSHAX <rafiehasannudin@gmail.com>
2026-04-16 20:09:05 +0700

    refactor: standardize string quotes and formatting in routes.js

--- src/utils/routes.js
+++ src/utils/routes.js
@@ -1,24 +1,21 @@
-import Express from "express";
-import { AppRoute } from "../routes/index.js";
-
-export function registerRoute(routes: Express.Router, route: AppRoute) {
+export function registerRoute (routes, route) {
   switch (route.method.toLowerCase()) {
-    case "get":
-      routes.get(route.path, route.handler);
-      break;
-    case "post":
-      routes.post(route.path, route.handler);
-      break;
-    case "put":
-      routes.put(route.path, route.handler);
-      break;
-    case "delete":
-      routes.delete(route.path, route.handler);
-      break;
-    case "patch":
-      routes.patch(route.path, route.handler);
-      break;
+    case 'get':
+      routes.get(route.path, route.handler)
+      break
+    case 'post':
+      routes.post(route.path, route.handler)
+      break
+    case 'put':
+      routes.put(route.path, route.handler)
+      break
+    case 'delete':
+      routes.delete(route.path, route.handler)
+      break
+    case 'patch':
+      routes.patch(route.path, route.handler)
+      break
     default:
-      throw new Error(`Unsupported HTTP method: ${route.method}`);
+      throw new Error(`Unsupported HTTP method: ${route.method}`)
   }
 }

09b3c132d304aebaf55be11073d6169bd7cc0f5a
GTPSHAX <rafiehasannudin@gmail.com>
2026-04-22 02:51:13 +0700

    refactor: streamline app initialization and configuration in index.js

--- src/index.js
+++ src/index.js
@@ -5,28 +5,29 @@ import Express from 'express'
 import ejs from 'ejs' // eslint-disable-line no-unused-vars
 
 import { loadRoutes } from './routes/index.js'
-configDotenv();
+configDotenv()
 
-(async () => {
-  const app = Express()
+const app = Express()
 
-  // Set EJS as the template engine
-  app.set('view engine', 'ejs')
+// Set EJS as the template engine
+app.set('view engine', 'ejs')
 
-  // Explicitly set the views directory
-  const dirname = import.meta.dirname
-  app.set('views', path.join(dirname, 'views'))
+// Explicitly set the views directory
+const dirname = import.meta.dirname
+app.set('views', path.join(dirname, 'views'))
 
-  app.use(Express.urlencoded({ extended: true }))
-  app.use(Express.json())
+app.use(Express.urlencoded({ extended: true }))
+app.use(Express.json())
 
-  // Load and register routes
-  const router = await loadRoutes()
-  app.use(router)
+// Load and register routes
+const router = await loadRoutes()
+app.use(router)
 
-  // Serving static files
-  app.use(Express.static('public'))
-  app.listen(3000, '0.0.0.0', () => {
-    console.log('Server is running on http://localhost:3000')
-  })
-})()
+// Serving static files
+app.use(Express.static('public'))
+
+app.listen(3000, '0.0.0.0', () => {
+  console.log('Server is running on http://localhost:3000')
+})
+
+export default app
